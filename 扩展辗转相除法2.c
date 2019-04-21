03d318138542e12748af9a04674595e052b5d48d
a751213964 <751213964@qq.com>
2019-04-21 19:58:29 +0800

    190421

--- 扩展辗转相除法2.c
+++ 扩展辗转相除法2.c
@@ -1,30 +1,25 @@
 /*************************************************************************
-	> File Name: 扩展辗转相除法.c
-	> Author: 
-	> Mail: 
-	> Created Time: 2019年04月21日 星期日 18时30分48秒
+	> File Name: 扩展辗转相除法2.c
+	> Author: ZhaoHang
+	> Mail: 751213964@qq.com
+	> Created Time: 2019年04月21日 星期日 19时39分48秒
  ************************************************************************/
+#include<stdio.h>
 
-#include <stdio.h>
-
-int x, y;
-int ex_gcd(int a, int b) {
+int ex_gcd(int a, int b, int *x, int *y) {
     if (b == 0) {
-        x = 1, y = 0;
+        *x = 1, *y = 0;
         return a;
     }
-    int r = ex_gcd(b, a % b), t = x;
-    x = y;
-    y = t - y * (a / b);
+    int r = ex_gcd(b, a % b, y, x);
+    *y = *y - *x * (a / b);
     return r;
 }
-
 int main() {
-    int n, m;
+    int n, m, x, y;
     while (~scanf("%d%d", &n, &m)) {
-        ex_gcd(n, m);
+        ex_gcd(n, m, &x, &y);
         printf("%d * %d + %d * %d = %d\n", n, x, m, y, n * x + m * y);
-
     }
     return 0;
 
